3e1dcfd35b9bddf7323014ff2a7164a59ee21a68
rofi <rfigueroa@truelogic.io>
2023-12-28 13:22:50 -0300

    x

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import EditScreenInfo from "@/components/EditScreenInfo";
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-thin">Tab One</Text>
+      <Text className="text-4xl font-thin">Tab One one</Text>
       <View className="my-8 bg-gray-200 h-px w-4/5" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
